4bdb77a61adf5349cc4c9d292efa7cc143bc72ac
Rebecca Jean Herman <rebecca.herman@dlr.de>
2024-09-11 16:05:23 +0200

    Added my iid plotting code! Next steps include making it work for TS graph and cleaning up the plotting functions

--- UnitlessDataGeneration.py
+++ UnitlessDataGeneration.py
@@ -16,6 +16,32 @@ from copy import deepcopy
 import matplotlib.pyplot as plt
 from matplotlib import patches as mpatches
 import re
+import scipy.stats
+
+class SortabilityPlotting():
+    @classmethod
+    def plot_sortability_dist(cls, sort_list, labels, title, legend_title="Edge Likelihood", fontsize=None):
+        for i, r2sort in enumerate(sort_list):
+            _ = plt.hist(r2sort, density=True, cumulative=False, bins=20, alpha=.5, label=labels[i])
+        plt.xlim([0,1])
+        plt.legend(title=legend_title, loc="upper right", fontsize=fontsize, title_fontsize=fontsize)
+        plt.title(title)
+    @classmethod
+    def plot_box(cls, d, pos, lc, fc, label, widths=.5):
+        bp1 = plt.boxplot(d, positions = pos, manage_ticks = False, patch_artist=True, showfliers=False, widths=widths)
+        for element in bp1.keys():
+                plt.setp(bp1[element], color=lc)
+        for patch in bp1['boxes']:
+                patch.set(facecolor=fc)
+        bp1['boxes'][0].set_label(label)
+    @classmethod
+    def plot_stat_dist(cls, r2s, title):
+        for i in range(r2s.shape[1]):
+            _ = plt.hist(r2s[:,i], density=True, cumulative=False, bins=20, alpha=.5, label=str(i))
+        plt.xlim([0,1])
+        plt.legend(title="Node")
+        plt.title(title)
+        plt.xlabel("R2 score")
 
 #Checks, warnings, and errors
 class UnstableError(Exception): pass
@@ -58,6 +84,118 @@ def _check_vars(now_vars, s_exp):
 def remove_diagonal(M):
     '''removes the diagonal from a 2D array M'''
     return M & ~np.diag(np.diag(M))
+
+def gen_unitless_iid_SCMs(N=20, O=100, B=5000, **kwargs):
+    Gs = [Graph(N, **kwargs).gen_coefficients() for i in range(B)]
+    for g in Gs:
+        g.gen_data(O)
+    return Gs
+
+def sortability_compare_collider_confounder(Ns = [i for i in range(3,22)], T = 500, B = 500):
+    def make_collider_adj(N):
+        A = np.zeros((N,N))
+        A[:-1,-1] = np.ones((N-1,))
+        return (A != 0)*1
+    def make_confounder_adj(N):
+        A = np.zeros((N,N))
+        A[0,1:] = np.ones((N-1,))
+        return (A != 0)*1
+    adj_type_dict = {"collider": make_collider_adj, "confounder": make_confounder_adj}
+        
+    r2dict = {}
+    keys = list(adj_type_dict.keys())
+    
+    for k in adj_type_dict.keys():
+        r2s  = np.empty((B,2, len(Ns)))*np.nan
+        for n, N in enumerate(Ns):
+            Gs = gen_unitless_iid_SCMs(N,T,B,**{'init_type': 'specified', 'init': adj_type_dict[k](N)})
+            r2s[:,:,n] = np.array([g.data.R2()[[0,-1]] for g in Gs]).squeeze()
+        r2dict[k] = r2s
+    plt.figure(figsize=(4,3))
+    SortabilityPlotting.plot_box(r2dict["confounder"][:,0,:], np.array(Ns)-.25-1, "blue", "skyblue", "confounder")
+    SortabilityPlotting.plot_box(r2dict["collider"][:,1,:], np.array(Ns)+.25-1, "red", "peachpuff", "collider")
+    plt.legend(loc="lower right")
+    plt.title("R2 scores for the hub node")
+    plt.xlabel("Number of Parents/Children")
+    plt.savefig(fname="HubR2", bbox_inches='tight')
+
+def sortability_compare_triple_types(O = 500, B = 10000):
+    adj_types = {}
+    adj_types["collider"] =np.array([[0,0,1],[0,0,1],[0,0,0]]); 
+    adj_types["chain"] =np.array([[0,1,0],[0,0,1],[0,0,0]]); 
+    adj_types["confounder"] =np.array([[0,1,1],[0,0,0],[0,0,0]]); 
+    
+    r2dict = {}
+    keys = list(adj_types.keys())
+
+    for k in keys:
+        Gs = gen_unitless_iid_SCMs(N=3, O=O, B=B, **{'init_type': 'specified', 'init': adj_types[k]})
+        r2dict[k] = np.array([g.data.R2() for g in Gs])
+
+    k = r2dict.keys()
+    F = plt.figure(figsize=(7,4.5))
+    ax = F.subplots(2,len(k))
+    i=1
+    for t, v in r2dict.items():
+        plt.subplot(2, len(k), i)
+        SortabilityPlotting.plot_stat_dist(v, t)
+        i+=1
+    i=0
+    for t in list(reversed(list(r2dict.keys()))):
+        plt.subplot(2,len(k),5)
+        _ = plt.hist(r2dict[t][:,i], density=True, cumulative=False, bins=20, alpha=.5, label=t)
+        plt.subplot(2,len(k),4)
+        _ = plt.hist(r2dict[t][:,(i+1)%3], density=True, cumulative=False, bins=20, alpha=.5, label=t)
+        i+=1
+    ts = ["lowest", "highest"]
+    yl_max = 0
+    for p in [4,5]:
+        yl_max = max(yl_max, plt.ylim()[1])
+    for p in [4, 5]:
+        plt.subplot(2,len(k),p)
+        plt.xlim([0,1])
+        plt.ylim([0, yl_max])
+        plt.legend()#loc = locs[p-1])
+        plt.title(ts[p-4]+"-scoring variable")
+        plt.xlabel("R2 score")
+    #plt.subplot(2, len(k), 2*len(k))
+    #SortabilityPlotting.plot_sortability_dist([r2sort, r2sort2], ["original", "modified"], "Random Graph", legend_title=None)
+    #plt.xlabel("R2-sortability")
+    #plt.legend(loc=(.6,.6))
+    F.tight_layout()
+    #plt.savefig(fname="triples")
+
+def sortability_compare_p(N=20, ps=[i/10 for i in range(1,11)], O=100, B=5000):
+    p_dict = {}
+    for p in ps:
+        Gs = gen_unitless_iid_SCMs(N=N, p=p, O=O, B=B)
+        varsort = [g.sortability() for g in Gs]
+        r2sort2  = [g.sortability('R2') for g in Gs]
+        p_dict[p] = (varsort, r2sort2)
+    ds = []
+    for i in range(2):
+        for func in [np.nanmean, scipy.stats.skew]:
+            ds += [np.array([scipy.stats.bootstrap((v[i],), func, n_resamples=99).bootstrap_distribution for _, v in p_dict.items()])]
+    plt.figure(figsize=(12,3))
+    plt.subplot(1,3,1)
+    SortabilityPlotting.plot_sortability_dist([v[0] for _, v in p_dict.items()], [k for k in p_dict.keys()], "Varsortability", fontsize="x-small")
+    plt.xlabel("Sortability")
+    plt.ylabel("Probability")
+    plt.subplot(1,3,2)
+    SortabilityPlotting.plot_sortability_dist([v[1] for _, v in p_dict.items()], [k for k in p_dict.keys()], "R2-sortability", fontsize="x-small")
+    plt.xlabel("Sortability")
+    plt.ylabel("Probability")
+    #_ = plt.hist(varsort, density=True, cumulative=False)#, bins=3)"#int(scipy.special.factorial(N)))
+    plt.subplot(1,3,3)
+    names = ["varsortability mean", "varsortability skew", "R2-sortability mean", "R2-sortability skew"]
+    colors = [("blue", "skyblue"), ("darkturquoise", "powderblue"), ("red", "peachpuff"), ("lightcoral", "mistyrose")]
+    plt.hlines([0, .5], 0, 1, colors=None, linestyles='--', label='_nolegend_')
+    for j in range(4):
+        SortabilityPlotting.plot_box(ds[j].T, np.array(list(p_dict.keys())), colors[j][0], colors[j][1], names[j], widths=.05)
+    plt.legend(loc=(.01, .6), fontsize="xx-small")
+    plt.xlabel("Edge Likelihood")
+    plt.title("Sortability Distribution Moments")
+    #plt.savefig(fname="SortabilityStats", bbox_inches='tight')
 
 def gen_unitless_time_series(N, tau_max, p=None, p_auto=None, T=1000, B=100, 
                              time_limit=5, init_type='no_feedback'):
@@ -182,6 +320,7 @@ class Graph(object):
                  "must match the number of variables {}").format(len(labels), self.N)
             self.labels = labels
         else:
+            #TODO: doesn't print right when i>9
             self.labels = ["$X_{}$".format(i) for i in self.variables]
         self._make_shape()
         _check_option('init_type', self.graph_types_, init_type)
